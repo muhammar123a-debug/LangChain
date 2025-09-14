be59cf1e4315ea23ef26e9296ef9b2119153b1fa
muhammar123a-debug <muhammar123a@gmail.com>
2025-09-14 11:32:16 +0500

    Add Intermediate task

--- Chain/IntermediateTask.py
+++ Chain/IntermediateTask.py
@@ -41,28 +41,55 @@ llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=os.getenv(
 # ✅ Task 2 Code – Problem → Solution Chain
 
 # Chain 1 --> Expand Problem
-problem_prompt = PromptTemplate(
-    input_variables = ["problem"],
-    template="The user has the problem: {problem}. Explain the problem in detailed, why it happen, and its impact"
-)
-problem_chain = LLMChain(llm=llm, prompt=problem_prompt, output_key="detailed_problem")
+# problem_prompt = PromptTemplate(
+#     input_variables = ["problem"],
+#     template="The user has the problem: {problem}. Explain the problem in detailed, why it happen, and its impact"
+# )
+# problem_chain = LLMChain(llm=llm, prompt=problem_prompt, output_key="detailed_problem")
 
-# Chain 2 --> Suggest Solution
-solution_prompt = PromptTemplate(
-    input_variables = ["detailed_problem"],
-    template = "Based on the following description, suggest 3 clear practical solutions: \n\n{detailed_problem}"
+# # Chain 2 --> Suggest Solution
+# solution_prompt = PromptTemplate(
+#     input_variables = ["detailed_problem"],
+#     template = "Based on the following description, suggest 3 clear practical solutions: \n\n{detailed_problem}"
+# )
+# solution_chain = LLMChain(llm=llm, prompt=solution_prompt, output_key="solutions")
+
+# # Sequential Chain
+# problem_solution = SequentialChain(
+#     chains=[problem_chain,solution_chain],
+#     input_variables = ["problem"],
+#     output_variables = ["detailed_problem","solutions"]
+# )
+
+# user_prompt = input("Enter your problem: ")
+# result = problem_solution({"problem": user_prompt})
+# print(result)
+
+
+# Chain 1 --> Character + Place Intro
+intro_Prompt = PromptTemplate(
+    input_variables = ["charactor","place"],
+    template = "Create a short introduction for a character named {charactor} who lives in {place}."
+) 
+intro_chain = LLMChain(llm=llm,prompt=intro_Prompt, output_key ="intro")
+
+# Chain 2 --> Story Builder
+story_prompt = PromptTemplate(
+    input_variable = ["intro"],
+    template="Based on the introduction below, write a short, engaging story:\n\n{intro}"
 )
-solution_chain = LLMChain(llm=llm, prompt=solution_prompt, output_key="solutions")
+story_chain = LLMChain(llm=llm,prompt=story_prompt, output_key="story")
 
 # Sequential Chain
-problem_solution = SequentialChain(
-    chains=[problem_chain,solution_chain],
-    input_variables = ["problem"],
-    output_variables = ["detailed_problem","solutions"]
+story_builder_chain = SequentialChain(
+    chains = [intro_chain,story_chain],
+    input_variables = ["charactor","place"],
+    output_variables = ["intro","story"]
 )
 
-user_prompt = input("Enter your problem: ")
-result = problem_solution({"problem": user_prompt})
+charactor = input("Enter you charactor: ")
+place = input("Enter your place: ")
+
+result = story_builder_chain({"charactor":charactor,"place":place})
+
 print(result)
-
-
