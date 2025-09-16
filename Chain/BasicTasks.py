fa21ff6e8e2c224439fc4161e69b1c82eb78bc8b
muhammar123a-debug <muhammar123a@gmail.com>
2025-09-16 09:48:01 +0500

    add more task

--- Chain/BasicTasks.py
+++ Chain/BasicTasks.py
@@ -7,18 +7,30 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
 
+# prompt = PromptTemplate(
+#     input_variables = ["text"],
+#     template="Summarize the following text in 2 line :\n\n{text}"
+# )
+
+# sumarize_chain = LLMChain(llm=llm, prompt=prompt)
+
+# text = """
+# Pakistan is a country in South Asia. It shares borders with India, China, Afghanistan, and Iran. 
+# It has a population of over 240 million people, making it the fifth most populous country in the world. 
+# Its capital city is Islamabad, and the largest city is Karachi.
+# """
+
+# result = sumarize_chain.run(text=text)
+# print(result)
+
+# Task 2: Translator Chain (Gemini ke sath)
 prompt = PromptTemplate(
-    input_variables = ["text"],
-    template="Summarize the following text in 2 line :\n\n{text}"
+    input_variables = ["english_text"],
+    template = "Translate the following English text into Urdu:\n\n{english_text}"
 )
+translate_chain = LLMChain(llm=llm, prompt=prompt)
 
-sumarize_chain = LLMChain(llm=llm, prompt=prompt)
+english_text = "Artificial Intelligence is changing the world by automating tasks and improving decision making"
 
-text = """
-Pakistan is a country in South Asia. It shares borders with India, China, Afghanistan, and Iran. 
-It has a population of over 240 million people, making it the fifth most populous country in the world. 
-Its capital city is Islamabad, and the largest city is Karachi.
-"""
-
-result = sumarize_chain.run(text=text)
+result = translate_chain.run(english_text=english_text)
 print(result)
