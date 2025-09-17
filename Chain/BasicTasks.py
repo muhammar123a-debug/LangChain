2ba761cd2e22e1db1fcf62c63a6340cf8b415954
muhammar123a-debug <muhammar123a@gmail.com>
2025-09-17 09:09:58 +0500

    add more task

--- Chain/BasicTasks.py
+++ Chain/BasicTasks.py
@@ -49,13 +49,81 @@ llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=os.getenv(
 # print(response)
 
 # Task 4: Tweet Generator Chain (Gemini ke sath)
-prompt=PromptTemplate(
-    input_variables = ["Topic"],
-    template = "Write a creative tweet about {topic}. Keep it under 280 characters and engaging",
+# prompt=PromptTemplate(
+#     input_variables = ["Topic"],
+#     template = "Write a creative tweet about {topic}. Keep it under 280 characters and engaging",
+# )
+# topic_chain = LLMChain(llm=llm, prompt=prompt)
+
+# topic = "AI in Education"
+
+# response = topic_chain.run(topic=topic)
+# print(response)\
+
+# Task 5: Pros & Cons Chain (Gemini ke sath)
+# prompt = PromptTemplate(
+#     input_variable = ['technology'],
+#     template = "List the pros and cons of the following technology:\n\n{technology}"
+# )
+# technology_chain = LLMChain(llm=llm,prompt=prompt)
+
+# technology = "BlockChain"
+
+# response = technology_chain.run(technology=technology)
+# print(response)
+
+# Task 6: Poem Generator Chain (Gemini ke sath)
+
+# prompt = PromptTemplate(
+#     input_variable = ["words"],
+#     template = "Write a short, creative poem using the following words: {words}"
+# )
+# word_chain = LLMChain(llm=llm,prompt=prompt)
+# words = "moon, river, silence, dream"
+# response = word_chain.run(words=words)
+# print(response)
+
+# Task 7: Code Writer Chain (Gemini ke sath)
+
+# prompt = PromptTemplate(
+#     input_variables = ["problem"],
+#     template = "Write clean Python code to solve the following problem:\n\n{problem}"
+# )
+# problem_chain = LLMChain(llm=llm,prompt=prompt)
+# problem = "Write a Python function to check if a number is prime."
+# response = problem_chain.run(problem=problem)
+# print(response)
+
+# Task 8: Grammar Fixer Chain (Gemini ke sath)
+# prompt = PromptTemplate(
+#     input_variables = ["sentences"],
+#     template="Fix the grammar of the following sentence and return the corrected version:\n\n{sentence}"
+# )
+
+# grammar_chain = LLMChain(llm=llm, prompt=prompt)
+# sentence = "She go to school yesterday and forget her book."
+# response = grammar_chain.run(sentence=sentence)
+# print(response)
+
+# Task 9: Story Expansion Chain (Gemini ke sath)
+
+# prompt = PromptTemplate(
+#     input_variable = ["short_story"],
+#     template="Expand the following short story into a detailed and engaging narrative:\n\n{short_story}"
+# )
+# chain = LLMChain(llm=llm,prompt=prompt)
+# short_story = "A boy found a key in the forest. He didn’t know what it could open"
+
+# result = chain.run(short_story=short_story)
+# print(result)
+
+# Task 10: FAQ Generator Chain (Gemini ke sath)
+
+prompt = PromptTemplate(
+    input_variable = ["product"],
+    template = "Generate 5 frequently asked questions with their answers about the product: {product}"
 )
-topic_chain = LLMChain(llm=llm, prompt=prompt)
-
-topic = "AI in Education"
-
-response = topic_chain.run(topic=topic)
+chain = LLMChain(llm=llm, prompt=prompt)
+product="Electric Scooter"
+response=chain.run(product=product)
 print(response)
